7428fcd5f4d6c47153d72c566b44124932b873b5
Olivier Le Guyader <olivier.leguyader@smallbard.com>
2019-09-17 15:31:00 +0100

    Enoncés et premiers exercices

---
+++ Corrections/Exercice.2.1.js
@@ -0,0 +1,4 @@
+var input = prompt('Veuillez entre un nombre');
+var number = parseInt(input, 10);
+var square = number * number;
+alert('Le carré de ce nombre est : ' + square);
---
+++ Corrections/Exercice.2.2.js
@@ -0,0 +1,11 @@
+var unitPrice = parseFloat(prompt("Veuillez saisir le prix unitaire de l'article"));
+var number = parseInt(prompt("Veuillez saisir le nombre d'articles"), 10);
+var VAT = parseFloat(prompt('Veuillez saisir le taux de TVA'));
+
+var price = unitPrice*number + unitPrice*number*VAT;
+
+alert('Le prix total est : ' + price.toFixed(2));
+
+function calculatePrice(unitPrice, numberOfUnit, VAT) {
+  return unitPrice*numberOfUnit + unitPrice*numberOfUnit*VAT;
+}
